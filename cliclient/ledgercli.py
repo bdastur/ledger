ad15793bfee638e28bb743b38d44b56ba07bf12c
Behzad Dastur <bdastur@gmail.com>
2016-03-19 15:02:52 +0000

    Add argparser for cli

--- cliclient/ledgercli.py
+++ cliclient/ledgercli.py
@@ -5,6 +5,7 @@
 Ledger CLI
 '''
 
+import argparse
 
 
 class LedgerCli(object):
@@ -13,8 +14,44 @@ class LedgerCli(object):
     '''
 
 
+def parse_arguments():
+    '''
+    Parse the command line arguments.
+    '''
+    parser = argparse.ArgumentParser(
+        prog="ledgercli.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='Ledger command line')
+    subparsers = parser.add_subparsers(
+        dest="operation",
+        help="sub commands")
+
+    # Summary.
+    showparser = subparsers.add_parser("show",
+                                       help="Show configuration")
+    showparser.add_argument("-f", "--format",
+                            choices=["text", "json"],
+                            required=False,
+                            help="Output format")
+    showparser.add_argument("-n", "--node",
+                            required=False,
+                            help="Show configuration for specific host")
+
+    showparser.add_argument("resource",
+                            help="Configuration names eg nova, keystone")
+
+
+
+    namespace = parser.parse_args()
+
+    return namespace
+
+
 def main():
-    pass
+    namespace = parse_arguments()
+    print namespace
+
+
 
 if __name__ == '__main__':
     main()
